c744480cbc5c9621878fcbeabbeb89c9b58fd817
Federico Rocchetti <federico_rocchetti@hotmail.com>
2022-07-14 23:59:12 -0400

    Solved

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,18 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+const addsZero = arr => {
+  for (let i = 0; i < arr.length; i++) {
+        for (let x = i+1; x < arr.length; x++) {
+            console.log(i, x)
+            if (arr[i] + arr[x] === 0) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+let checkAddsZero = addsZero(array)
+console.log(checkAddsZero)
